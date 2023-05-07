ab70b4526f2c22f772f1e2b51df38d600706d613
Allen Lau <alau002@citymail.cuny.edu>
2023-05-07 01:48:26 -0400

    query logic with keywords

--- flask/app.py
+++ flask/app.py
@@ -1,34 +1,12 @@
 from flask import Flask, request, render_template
-import mysql.connector
-import backend.populate_database as populate
+import backend.populate_database as populate_db
+import backend.query_database as query_db
 import backend.config as config
 
 #creating flask app instance
 app = Flask(__name__)
 
-#function for connecting the database
-def get_db_connection():
-    mySQLparams = config.mySQLparams
-    connection = mysql.connector.connect(
-        host = mySQLparams['host'],
-        user = mySQLparams['user'],
-        password = mySQLparams['password'],
-        database = mySQLparams['database']
-    )
-    return connection
-
-
-#function for performing search in the mysql database using match function 
-def search(query):
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute('SELECT url, website_title, LEFT(raw_text, 300)  FROM search_results WHERE MATCH (raw_text) AGAINST(\''+query+'\' IN NATURAL LANGUAGE MODE)' )     #text is limited to 300 character from raw text
-    results = cursor.fetchall()
-    cursor.close()
-    connection.close()
-
-    return results
-
+#entry to search_engine 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -38,9 +16,9 @@ def index():
         engines = config.engines.keys()
         #loop through each engine and populate the database based on the query and engine
         for engine in engines:
-            populate.populate_database(query,engine)
+            populate_db.populate_database(query,engine)
         #get the search results
-        results = search(query)
+        results = query_db.search(query)
         #display HTML
         return render_template('results.html', results=results)
     return render_template('index.html')
